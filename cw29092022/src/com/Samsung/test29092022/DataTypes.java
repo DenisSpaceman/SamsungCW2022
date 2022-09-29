b5bef9410d4f9f249a2a39ebfb6a399fd6a9a387
DenisSpaceman <den16042005@gmail.com>
2022-09-29 19:52:58 +0300

    Update DataTypes.java

--- cw29092022/src/com/Samsung/test29092022/DataTypes.java
+++ cw29092022/src/com/Samsung/test29092022/DataTypes.java
@@ -1,7 +1,19 @@
 package com.Samsung.test29092022;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class DataTypes {
     public static void main(String[] args) {
-        
+
+        byte aByte = -128;
+        short aShort = 32676;
+        int aInt = 2147483647;
+        long aLong = 2000000000L;
+        double aDouble = 0.1;
+        float aFloat = 0.1f;
+
+        Scanner in = new Scanner(System.in);
+
+        in.useLocale(Locale.US); // смена формата ввода данных
     }
 }
