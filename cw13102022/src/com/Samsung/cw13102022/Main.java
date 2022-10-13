9a47696b37ed62846400c4749748b7a177c7eaaf
DenisSpaceman <den16042005@gmail.com>
2022-10-13 20:59:40 +0300

    Update Main.java

--- cw13102022/src/com/Samsung/cw13102022/Main.java
+++ cw13102022/src/com/Samsung/cw13102022/Main.java
@@ -2,11 +2,40 @@ package com.Samsung.cw13102022;
 
 public class Main {
     public static void main(String[] args) {
-        int i = 0;
+        int ai = 0;
 
-        while (i < 100){
+        /*
+        while (ai < 100){
             System.out.println(i);
-            i++;
+            ai++;
         }
+        */
+        /*
+        for (int i = 0; i < 100; i++){
+            System.out.println(i);
+        }
+         */
+        /*
+        for (;;){
+            System.out.println(1);
+            if () break;
+        }
+         */
+
+        int [] array;
+        array = new int[10];
+
+        array[9] = 10;
+        array[0] = -100;
+
+        double res = 0;
+
+        for (int i = 0; i < 10; i++){
+            array[i] = (int) (Math.random() * 100);
+            System.out.print(array[i] + " ");
+            res += array[i];
+        }
+
+        System.out.println("\n" + res / array.length);
     }
 }
