cd3ee76e010d18c38a254d6aded8f551bf12fd56
DenisSpaceman <den16042005@gmail.com>
2022-10-13 19:23:15 +0300

    Create Main.java

---
+++ cw13102022/src/com/Samsung/cw13102022/Main.java
@@ -0,0 +1,12 @@
+package com.Samsung.cw13102022;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i < 100){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
